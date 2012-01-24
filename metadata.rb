af60580cf1c9db7b52a28e55fca71093b9053f36
David Amian <damian@emergya.com>
2012-01-24 18:48:25 +0100

    Fix validations

--- metadata.rb
+++ metadata.rb
@@ -73,6 +73,7 @@ attribute 'printers/printers/ppd_uri',
   :description  => "Set the uri of ppd if is necessary download it",
   :type         => "string",
   :order        => "4",
+  :validation   => "url",
   :recipes      => [ 'printers::printers' ]
 
 attribute 'printers/printers/uri',
@@ -81,4 +82,5 @@ attribute 'printers/printers/uri',
   :type         => "string",
   :required     => "required",
   :order        => "5",
+  :validation   => "printer_uri,
   :recipes      => [ 'printers::printers' ]
